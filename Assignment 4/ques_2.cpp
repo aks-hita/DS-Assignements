57905f670ed84dce96b707029172dfce0695bd57
aks-hita <bhar.akshita28@gmail.com>
2025-09-17 11:29:14 +0530

    Add files via upload

---
+++ Assignment 4/ques_2.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+using namespace std;
+#define MAXLEN 5
+class queue {
+    int q[MAXLEN];
+    int front,rear;
+    public:
+    void init(){
+        front=rear=-1;
+    }
+    int isEmpty(){
+        if(rear==-1)
+            return 1;
+        else
+            return 0;
+        }
+    int isFull(){
+        if(front==(rear+1)%MAXLEN)
+            return 1;
+        else
+            return 0;
+    }
+    int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        else
+            return q[front];
+    }
+    void enqueue(int n){
+        if(isFull()){
+           cout<<"Error: Overflow."<<endl;
+        }
+        else if(isEmpty()){
+            front=rear=0;
+        }
+        else{
+            rear=(rear+1)%MAXLEN;
+        }
+         q[rear]=n;
+    }
+    int dequeue(){
+        int val=-1;
+        if(!isEmpty()){
+            val=peek();
+            if(front==rear)
+                front=rear=-1;
+            else
+                front=(front+1)%MAXLEN;
+        }
+        return val;
+    }
+    void display(){
+        if(isEmpty()){
+                cout<<"The queue is empty.";
+            }
+        else{
+            int i=front;
+            cout<<"The Queue: ";
+            while(true){
+                cout<<q[i]<<" ";
+                if(i==rear)
+                    break;
+                else
+                i=(i+1)%MAXLEN;
+            }
+            cout<<endl;
+        }
+    }
+};
+int main(){
+    int a,opt;
+    queue q1;
+    q1.init();
+    do{
+        cout<<endl<<"Choose an option: "<<endl<<"1. enqueue\n2. dequeue\n3. isEmpty\n4. isFull\n5. display\n6. peek\n7. exit ";
+        cin>>opt;
+        switch (opt){
+        case 1:{
+            cout<<"Enter a number ";
+            int num;
+            cin>>num;
+            q1.enqueue(num);
+            break; }
+        case 2:{
+            int val=q1.dequeue();
+            if(val!=-1){
+                cout<<"The dequeued value is "<<val<<endl;
+            }
+            else
+                cout<<"The queue is empty.";
+            break;}
+        case 3:{
+            if(q1.isEmpty()){
+                cout<<"The queue is empty.";
+            }
+            else{
+                cout<<"The queue is not empty.";
+            }
+            break;}
+        case 4:{
+        if(q1.isFull()){
+                cout<<"The queue is full.";
+            }
+            else{
+                cout<<"The queue is not full.";
+            }
+            break;}
+        case 5:{
+            q1.display();
+            break;}
+        case 6:{
+            int val1=q1.peek();
+            if(val1!=-1){
+                cout<<"The front value is "<<val1<<endl;
+                }
+            break;}
+        case 7:{
+            cout<<"Thank you!";
+            break;}
+        }
+    }
+    while(opt!=7);
+    return 0;
+}
